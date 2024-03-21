d44941f54c95fc826095471b94df7c0e662d965d
vexfairyprincess <samora@uamv.edu.ni>
2024-03-21 13:20:00 -0600

    first commit

---
+++ listas.py
@@ -0,0 +1,2 @@
+datos = [21, "hola", 45, 33, "total", 7]
+print("Imprimiendo el primer elemento de la lista: ", datos[-1])
---
+++ whileif.py
@@ -0,0 +1,4 @@
+buscar = int(input('Digite el número a buscar: '))
+cont = 20
+while cont <= 50:
+    if cont == buscar
